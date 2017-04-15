732bac9ab76f56281f7bf45c55774d1966be3a02
Vsevolod Rodionov <Vsevolod_Rodionov@epam.com>
2017-04-16 00:21:39 +0300

    removing redundant "value" class

--- florida/tensors/index.js
+++ florida/tensors/index.js
@@ -1,6 +1,5 @@
 //@flow
 export {MemoryCell} from './MemoryCell';
-export {Value} from './Value';
 export {Placeholder} from './Placeholder';
 
 export {OndemandComputationTensor} from './OndemandComputationTensor';
